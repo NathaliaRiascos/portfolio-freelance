41ad719136552fcbd6e40ddf792f7875d6ae4f6c
NathaliaRiascos <riascosnathalia6@gmail.com>
2025-08-08 18:01:44 -0500

    feat: add footer section

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -2,3 +2,4 @@ export { default as Hero } from "./Hero.astro";
 export { default as Services } from "./Services.astro";
 export { default as Projects } from "./Projects.astro";
 export { default as Contact } from "./Contact.astro";
+export { default as Footer } from "./Footer.astro";
